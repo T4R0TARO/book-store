e4b847365d20f6f656cf4b09ead1ce2cbac3644f
josh manansala <joshmanansalagsd@gmail.com>
2024-01-06 18:51:14 -0800

    feature: connect to MONGODB

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,1 +1,11 @@
 // connect to MONGODB
+import mongoose from "mongoose";
+
+const connectDB = (url) => {
+  return mongoose
+    .connect(url)
+    .then(() => console.log("CONNECTED TO DB..."))
+    .catch((error) => console.log(error));
+};
+
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,17 @@
-// const express = require("express");
+// require("dotenv").config();
+import { config } from "dotenv";
+config({ path: "../.env" });
 import express from "express"; // REFACTORED because of ` 'type':'module' `
+import connectDB from "./db/connect.js";
+
 const app = express();
-
-// connect to DB
-// routes
-// error handler
-
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}...`);
     });
