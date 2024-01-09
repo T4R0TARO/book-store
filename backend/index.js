cc14772b808dcb1930caa9ad737553221ca34def
josh manansala <joshmanansalagsd@gmail.com>
2024-01-08 18:33:15 -0800

    add: cors policy middleware

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,23 @@ config({ path: "../.env" });
 import express from "express"; // REFACTORED because of ` 'type':'module' `
 import connectDB from "./db/connect.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 // parse JSON files
 app.use(express.json());
+
+//Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (request, response) => {
   // console.log(request);
