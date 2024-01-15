450012bef1b140141f0f835eaec73b281e9804a5
josh manansala <joshmanansalagsd@gmail.com>
2024-01-15 14:18:10 -0800

    clean up comments and dead code

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -32,29 +32,6 @@ const getAllBooks = async (request, response) => {
 };
 // Update Book by Id
 const updateBook = async (request, response) => {
-  // try {
-  //   if (
-  //     !request.body.title ||
-  //     !request.body.author ||
-  //     !request.body.publishYear
-  //   ) {
-  //     return response.status(400).send({
-  //       message: "Send all required fields: title, author, publishYear",
-  //     });
-  //   }
-  //   const { id } = request.params;
-  //   const result = await Book.findByIdAndUpdate(id, request.body);
-  //   if (!result) {
-  //     return response.status(404).json({ message: "Book not found..." });
-  //   }
-  //   return response
-  //     .status(200)
-  //     .send({ message: "Book updated successfully..." });
-  // } catch (error) {
-  //   console.log(error.message);
-  //   response.status(500).send({ message: error.message });
-  // }
-
   const { id } = request.params;
   const result = await Book.findByIdAndUpdate(id, request.body);
   if (!result) {
