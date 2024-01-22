fb439008cd8710393efa623806cfa41eb1dd3925
josh manansala <joshmanansalagsd@gmail.com>
2024-01-22 05:58:55 -0800

    fix: DOM error warning

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,15 +35,17 @@ const Home = () => {
       ) : (
         <table className="w-full border-separate border-spacing-2">
           <thead>
-            <th className="border border-slate-600 rounded-md">No</th>
-            <th className="border border-slate-600 rounded-md">Title</th>
-            <th className="border border-slate-600 rounded-md max-md:hidden">
-              Author
-            </th>
-            <th className="border border-slate-600 rounded-md max-md:hidden">
-              Publish Year
-            </th>
-            <th className="border border-slate-600 rounded-md">Operations</th>
+            <tr>
+              <th className="border border-slate-600 rounded-md">No</th>
+              <th className="border border-slate-600 rounded-md">Title</th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
+                Author
+              </th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
+                Publish Year
+              </th>
+              <th className="border border-slate-600 rounded-md">Operations</th>
+            </tr>
           </thead>
           <tbody>
             {books.map((book, index) => (
