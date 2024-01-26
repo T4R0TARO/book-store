592313ff6f6c8ecb8ed34d3fc3961b65881046aa
josh manansala <joshmanansalagsd@gmail.com>
2024-01-26 13:49:45 -0800

    refactor: BooksCard refactored into separate component BooksSingleCard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
 // import { AiOutlineEdit } from "react-icons/ai";
 // import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineAddBox /*, MdOutlineDelete*/ } from "react-icons/md";
 import BooksCard from "../components/home/BooksCard";
 import BooksTable from "../components/home/BooksTable";
 
