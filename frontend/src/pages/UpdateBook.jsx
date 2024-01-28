d5ff62e91738a17b06d98e17bfa873b219fa8dc4
josh manansala <joshmanansalagsd@gmail.com>
2024-01-28 14:42:26 -0800

    feature: notistack notifications

--- frontend/src/pages/UpdateBook.jsx
+++ frontend/src/pages/UpdateBook.jsx
@@ -3,6 +3,7 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const UpdateBook = () => {
   const [title, setTitle] = useState("");
@@ -11,6 +12,8 @@ const UpdateBook = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
+
   useEffect(() => {
     setLoading(true);
     axios
@@ -39,11 +42,13 @@ const UpdateBook = () => {
       .put(`http://localhost:3000/api/v1/books/${id}`, data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar("Book Edited Successfully"), { variant: "success" };
         navigate("/");
       })
       .catch((error) => {
         setLoading(false);
-        alert("An error happended... Please check console");
+        // alert("An error happended... Please check console");
+        enqueueSnackbar("Error", { variant: "error" });
         console.log(error);
       });
   };
